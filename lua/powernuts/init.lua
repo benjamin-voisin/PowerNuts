6d05f0f3b3797e4a97cdda96f76f9a1f460d7a7d
Benjamin V <benjamin.voisin@ens-rennes.fr>
2023-09-17 12:01:30 +0200

    ✨ The :Nuts command now run the squirrel prover on the current file
    :Nuts opens a floating terminal printing the output of running the
    squirrel prover on the current file.

--- lua/powernuts/init.lua
+++ lua/powernuts/init.lua
@@ -1,9 +1,13 @@
 local powernuts = {}
 
+function powernuts.sayNuts()
+    require("toggleterm").setup()
+    local Terminal  = require('toggleterm.terminal').Terminal
+    local command =  powernuts.path..' '..vim.fn.expand('%')
+    local squirrel = Terminal:new({ cmd = command, close_on_exit=false, direction="float"})
+    squirrel:toggle()
 
-
-
-function powernuts.sayNuts() print('I like Nuts !') end
+end
 
 function powernuts.sayPath() print(powernuts.path or 'No Path to the squirrel prover specified') end
 
