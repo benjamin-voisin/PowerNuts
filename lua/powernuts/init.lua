d147bd96a77101ed8f1fe865f5fb2e83c7e93c71
Benjamin V <benjamin.voisin@ens-rennes.fr>
2023-09-22 10:36:41 +0200

    ✨ :UnNut command to close created windows
    The UnNut command closes the windows created by PowerNuts (goal0 and info0), and wipe
    their buffers. If you try to leave nvim without this command it will ask you to save
    the changes made to the windows (changes that you don't want to save).

--- lua/powernuts/init.lua
+++ lua/powernuts/init.lua
@@ -1,14 +1,56 @@
 local powernuts = {}
 
+-- vim.cmd(":autocmd BufEnter * if tabpagenr('$') == 2 && winnr('$') == 2 && exists('b:info0') && exists('b:goal0') | quit | endif")
+
+function powernuts.setup(table)
+    powernuts.path = table["path"] or "No path to the squirrel prover specified"
+end
+
+local function get_buf(name)
+    local buffers = vim.api.nvim_list_bufs()
+    for i = 1, #buffers, 1 do
+        if (string.sub(vim.api.nvim_buf_get_name(buffers[i]), -(#name)) == name) then
+            return buffers[i]
+        end
+    end
+end
+
+local function get_win(name)
+    local windows = vim.api.nvim_list_wins()
+    for i = 1, #windows, 1 do
+        if (string.sub(vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(windows[i])), -(#name)) == name) then
+            return windows[i]
+        end
+    end
+end
+
+local function get_ascii(path)
+    local lines = {}
+    for line in io.lines(path) do
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
+
 function powernuts.sayNuts()
     require("toggleterm").setup()
     local Terminal  = require('toggleterm.terminal').Terminal
     local command =  powernuts.path..' '..vim.fn.expand('%')
     local squirrel = Terminal:new({ cmd = command, close_on_exit=false, direction="float"})
-    squirrel:toggle()
+    vim.cmd(":set splitright | vnew info0 |  new goal0 | winc h")
+    -- squirrel:toggle()
+    -- print(vim.api.nvim_buf_get_name(vim.api.nvim_list_wins()[1]))
+
+    vim.api.nvim_buf_set_text(get_buf("info0"), 0, 0, 0, 0, get_ascii("assets/squirrels.txt"))
 
 end
 
-function powernuts.sayPath() print(powernuts.path or 'No Path to the squirrel prover specified') end
+function powernuts.unNut()
+    vim.api.nvim_buf_delete(get_buf("goal0"), {force = true})
+    vim.api.nvim_buf_delete(get_buf("info0"), {force = true})
+end
+
+function powernuts.sayPath() print(powernuts.path) end
 
 return powernuts
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,4 +1,6 @@
 vim.api.nvim_create_user_command('Nuts', 'lua require("powernuts").sayNuts()', {})
 vim.api.nvim_create_user_command('NutsPath', 'lua require("powernuts").sayPath()', {})
 
+vim.api.nvim_create_user_command('UnNut', 'lua require("powernuts").unNut()', {})
+
 
