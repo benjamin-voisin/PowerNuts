65f67ae5c961c3d3c4424b3a586dec919ae9d213
Benjamin V <benjamin.voisin@ens-rennes.fr>
2024-02-04 11:05:58 +0100

    ⚰️  remove lua code

--- lua/powernuts/helloworld.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-function M.sayNuts() print('I like Nuts !') end
-
-function M.sayPath(path) print(path) end
-
-return M
--- plugin/init.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_user_command('Nuts', 'lua require("powernuts").sayNuts()', {})
-vim.api.nvim_create_user_command('NutsPath', 'lua require("powernuts").sayPath()', {})
-
-vim.api.nvim_create_user_command('UnNut', 'lua require("powernuts").unNut()', {})
-
-
